01a4881416af9a37031fd655488c6dccc8f32db4
krunalbinekar <65150066+krunalbinekar@users.noreply.github.com>
2023-03-17 11:40:56 +0530

    Removed authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   // {path: '**', redirectTo: 'home', pathMatch: 'full'},
   {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
-  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule),canActivate : [AuthGuard]},
+  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
 
 
 ];
